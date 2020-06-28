40692bfc0f79bd39630e5c057f60bc37f8724cd1
ass3 <sabirsaira00@gmail.com>
2020-06-28 12:44:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/applimad/signin.java
@@ -0,0 +1,20 @@
+package com.example.applimad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+    public void btn_signup(View view) {
+        startActivity(new Intent(getApplicationContext(),signup.class));
+    }
+}
---
+++ app/src/main/java/com/example/applimad/signup.java
@@ -0,0 +1,23 @@
+package com.example.applimad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void btn_signin(View view) {
+
+        startActivity(new Intent(getApplicationContext(),signin.class));
+
+    }
+
+}
